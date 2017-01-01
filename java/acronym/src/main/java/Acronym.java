51f6cb389a725b37409793a62b78c74c87903805
Vishal <try@try>
2017-01-01 14:49:55 +0530

    Acronym modified add hyphen filter and space splitter for acronym

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -1,6 +1,12 @@
 public class Acronym {
 
-  public static String generate(String phrase) {git
-
+  public static String generate(String phrase) {
+    phrase = phrase.replaceAll("-"," ");
+    String phraseArray[] = phrase.split(" ");
+    StringBuilder acronym = new StringBuilder();
+    for(int i=0 ; i< phraseArray.length; i++) {
+      acronym.append(phraseArray[i].toCharArray()[0]);
+    }
+     return acronym.toString().toUpperCase();
   }
 }
